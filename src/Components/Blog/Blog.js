4b0352bd9c9b7d00cc29d50ead9da5c220ba41fb
MarinhSt <st_marinh@protonmail.com>
2022-11-05 00:15:39 -0500

    The Blog component was modified to show previews of the posts and to be able to go to them. I have created the BlogCard component and the array of the posts data.

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,27 +1,49 @@
+import { NavLink } from 'react-router-dom'
+
 function Blog() {
     return (
         <>
             <h1>Blog</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                facilis sunt fuga porro eum ad esse temporibus dolor? Neque
-                corporis incidunt, perferendis numquam sequi eveniet sed aut
-                rem. Harum, enim?
+                Welcome to our blog. here you can find some posts form your
+                interest
             </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                facilis sunt fuga porro eum ad esse temporibus dolor? Neque
-                corporis incidunt, perferendis numquam sequi eveniet sed aut
-                rem. Harum, enim?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                facilis sunt fuga porro eum ad esse temporibus dolor? Neque
-                corporis incidunt, perferendis numquam sequi eveniet sed aut
-                rem. Harum, enim?
-            </p>
+            <ul>
+                {blogData.map(post => (
+                    <BlogCard post={post} />
+                ))}
+            </ul>
         </>
     )
 }
+
+function BlogCard({ post }) {
+    return (
+        <li>
+            <NavLink to={post.slug}>{post.tittle}</NavLink>
+            <p>{post.author}</p>
+        </li>
+    )
+}
+
+const blogData = []
+blogData.push({
+    slug: 'react',
+    tittle: 'What is React.js?',
+    text: 'is a free and open-source front-end JavaScript library for building user interfaces based on UI components.',
+    author: 'By: My other me',
+})
+blogData.push({
+    slug: 'vue',
+    tittle: 'What is Vue.js?',
+    text: 'Vue.js is a model-view-view-model (MVVM) JavaScript framework for building user interfaces (UIs) and single-page applications.',
+    author: 'By: Me',
+})
+blogData.push({
+    slug: 'react',
+    tittle: 'What is Next.js?',
+    text: 'is a JavaScript framework that enables you to build superfast and extremely user-friendly static websites, as well as web applications using React.',
+    author: 'By: Someone else',
+})
 
 export { Blog }
